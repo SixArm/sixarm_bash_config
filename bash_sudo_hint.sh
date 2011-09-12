b7c3fbbdd18138d0245bfe8a1641e73d2c91f5d1
SixArm <sixarm@sixarm.com>
2011-09-11 20:13:25 -0700

    Fix EOF and indents.

--- bash_sudo_hint.sh
+++ bash_sudo_hint.sh
@@ -4,10 +4,9 @@ if [ ! -e "$HOME/.sudo_as_admin_successful" ]; then
     case " $(groups) " in *\ admin\ *)
     if [ -x /usr/bin/sudo ]; then
         cat <<EOF
-        To run a command as administrator (user "root"), use "sudo <command>".
-        See "man sudo_root" for details.
-
-        EOF
+To run a command as administrator (user "root"), use "sudo <command>".
+See "man sudo_root" for details.
+EOF
     fi
     esac
 fi
