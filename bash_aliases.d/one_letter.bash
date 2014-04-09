2c3c54993f8b2057f63e6c3281a9306d3e9a851d
SixArm <sixarm@sixarm.com>
2014-04-08 18:48:54 -0700

    Move "g" to "g_for_git"

--- bash_aliases.d/one_letter.bash
+++ bash_aliases.d/one_letter.bash
@@ -9,9 +9,6 @@ hash emacs 2>&- && alias e='emacs -nw --color=no' || alias e='vi'
 # f = find fast
 hash ack-grep 2>&- && alias f='ack-grep' || hash ack 2>&- && alias f='ack' || alias f='find . | xargs grep '
 
-# g = git
-alias g='git'
-
 # m = most || more
 hash most 2>&- && alias m='most' || alias m='more'
 
