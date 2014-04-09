e82823c4f095ed3a9ad3c3b8e1d4926276951fa4
SixArm <sixarm@sixarm.com>
2014-04-08 18:58:59 -0700

    Refactor all one-letter aliases to their own files

--- bash_aliases.d/one_letter.bash
+++
@@ -1,8 +0,0 @@
-## One Letter Shortcuts
-
-# f = find fast
-hash ack-grep 2>&- && alias f='ack-grep' || hash ack 2>&- && alias f='ack' || alias f='find . | xargs grep '
-
-# m = most || more
-hash most 2>&- && alias m='most' || alias m='more'
-
