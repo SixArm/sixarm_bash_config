953dafeac701e01096ce2c0e87fac6147073a12f
SixArm <sixarm@sixarm.com>
2014-04-08 18:53:15 -0700

    Move "e" to "e_for

--- bash_aliases.d/one_letter.bash
+++ bash_aliases.d/one_letter.bash
@@ -1,8 +1,5 @@
 ## One Letter Shortcuts
 
-# e = edit using emacs || vi
-hash emacs 2>&- && alias e='emacs -nw --color=no' || alias e='vi'
-
 # f = find fast
 hash ack-grep 2>&- && alias f='ack-grep' || hash ack 2>&- && alias f='ack' || alias f='find . | xargs grep '
 
