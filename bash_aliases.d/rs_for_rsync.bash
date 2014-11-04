a38a1a466b74434ae1afe0b70e20a0651341abac
Joel Parker Henderson <joel@joelparkerhenderson.com>
2014-11-03 22:42:29 -0700

    Align bash rsync with zsh rsync

--- bash_aliases.d/rs_for_rsync.bash
+++
@@ -1,8 +0,0 @@
-# rs: rsync with settings for typical mirroring.
-#
-# We do not use the --partial flag because sometimes 
-# we will not have partial-write capability due to
-# directory permissions.
-#
-alias rs='rsync --archive --compress --progress --verbose'
-
---
+++ bash_aliases.d/rsync.bash
@@ -0,0 +1,9 @@
+# Rsync aliases
+
+# rs => rsync with settings for typical mirroring
+alias rs='rsync -aHuOxzvi'
+
+# rsp => rsync with progress bar and and partial flags,
+# suitable for fast resume of any broken transfers;
+# the partial flags need directory write permission.
+alias rsp='rsync -aHuOxzvi --progress --partial --partial-dir=.rsync-partial-dir'
