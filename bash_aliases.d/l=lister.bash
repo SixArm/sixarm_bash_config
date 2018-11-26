c739c7598496e957157bfb047f84b79da62a838b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-11-26 09:10:18 -0800

    Drop extended attributes

--- bash_aliases.d/l=lister.bash
+++ bash_aliases.d/l=lister.bash
@@ -1,3 +1,3 @@
 # l = lister of files
-{ hash exa  2>&- && alias l='exa' & alias ll='exa --long --bytes --extended --group  --time-style long-iso --git'; } ||
+{ hash exa  2>&- && alias l='exa' & alias ll='exa --long --bytes --group --time-style long-iso --git'; } ||
 { hash ls 2>&- && alias l='ls' & alias ll='ls -1A'; }
