d7e72ac589b3fd55faead8f8392c1d69e0ba1a0b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-11-26 09:06:18 -0800

    Add exa features

--- bash_aliases.d/l=ls.bash
+++
@@ -1,4 +0,0 @@
-# l: list files                                                                                                                                                                 
-alias l='ls -1AF'                                                                                                                                                             
-alias ll='ls -lA'                                                                                                                                                             
-
